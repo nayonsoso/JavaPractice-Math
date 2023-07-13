9fb2b9f75f9a4657c9e7da9d52fe39d9f384d2d0
nayonsoso <panda0329@naver.com>
2023-07-13 10:58:31 +0900

    230713

---
+++ src/palindrome/Main.java
@@ -0,0 +1,39 @@
+package palindrome;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int times = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < times; i++) {
+            char[] arr = br.readLine().toCharArray();
+            int l = arr.length;
+            System.out.println(check(arr, 0, arr.length-1, 0));
+        }
+    }
+
+    public static int check(char[] arr, int left, int right, int cnt){
+        // System.out.printf("left:%d right:%d cnt:%d, arr:%s\n", left, right,cnt, Arrays.toString(arr));
+        if(left>=right){
+            return cnt;
+        }
+
+        if(arr[left]==arr[right]){
+            cnt = check(arr, left+1, right-1, cnt);
+        } else {
+            if(cnt==0){
+                int cnt1 = check(arr, left, right-1,cnt+1);
+                int cnt2 = check(arr, left+1, right,cnt+1);
+                cnt = Math.min(cnt1, cnt2);
+            } else if(cnt==1){
+                cnt = cnt+1;
+            }
+        }
+        return cnt;
+    }
+}
---
+++ src/palindromeByRecursion/Main.java
@@ -0,0 +1,4 @@
+package palindromeByRecursion;
+
+public class Main {
+}
