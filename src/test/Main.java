e7f3a0ddce1540c1ddcad985808093ede71cf07b
nayonsoso <panda0329@naver.com>
2023-07-14 15:47:29 +0900

    230714

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,74 +1,24 @@
 package test;
-// 하~중
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<> ll = new LinkedList()<>;
-        ll.re
-    }
-}
-
-
-class Solution {
-    public int solution(String[] ingredients, String[] items) {
-        //ingredients = new String[]{"1", "2"};
-        //items = new String[]{"1"};
-
-        int left = 0;
-        int right = items.length-1;
-        LinkedList<String> ingredlist = new LinkedList<>(Arrays.asList(ingredients));
-        LinkedList<String> itemList = new LinkedList<>(Arrays.asList(items));
-
-        // System.out.printf("left: %d, right: %d\n",left, right);
-        return check(items, ingredList, itemList, left, right);
-    }
-
-    // left부터 right까지 영역을 구하고, 모든 ingred가 그 안에 속하는지를 파악한다음, true이면 구간의 길이를 리턴
-    public int check(String[] items, LinkedList<String> ingredList, LinkedList<String> itemList, int left, int right){
-        boolean containsLeft = ingredlist.contains(items[left]);
-        boolean containsRight = ingredList.contains(items[right]);
-
-        while(!(containsLeft && containsRight) || left <= right){
-            // System.out.printf("left: %d, right: %d, containsLeft: %b, containsRight: %b\n",left,right, containsLeft, containsRight);
-            if(containsLeft){
-                containsRight = igredList.contains(items[--right]);
-                itemList.deleteLast();
-            } else if (containsRight){
-                containsLeft = igredList.contains(items[++left]);
-                itemList.deleteFirst();
-            } else {
-                containsRight = igredList.contains(items[--right]);
-                containsLeft = igredList.contains(items[++left]);
-                itemList.deleteLast();
-                itemList.deleteFirst();
+        int[] arr = {5, 3, 1, 4, 6, 1};
+        int[] visited = new int[arr.length];
+        int visitCnt = 0;
+        int minVal = Integer.MAX_VALUE;
+        int minIdx = -1;
+        while(visitCnt < arr.length) {
+            for(int i=0; i<arr.length; i++){
+                if(arr[i]<minVal && visited[i]==0){
+                    minVal = arr[i];
+                    minIdx = i;
+                }
             }
-        }
-
-        if(left > right){
-            return 0;
-        }
-
-        boolean check = true;
-        for(int i=0; i<left; i++){
-            if(!itemList.contains(ingredList.get(i))){
-                check=false;
-                break;
-            }
-        }
-
-        if(check){
-            int hoksi1 = check(items, ingredList, itemList, left+1, right);
-            int hoksi2 = check(items, ingredList, itemList, left, right-1);
-            if(Math.max(hoksi1, hoksi2) == 0){
-                return right-left+1;
-            } else {
-                return Math.max(hoksi1, hoksi2);
-            }
-        } else {
-            return 0;
+            System.out.print(minVal + " ");
+            visited[minIdx] = 1;
+            visitCnt++;
+            minVal = Integer.MAX_VALUE;
+            minIdx = -1;
         }
     }
 }
-
-// ingrediet가 모두 포함된 가장 짧은 배열의 구간을 출력
